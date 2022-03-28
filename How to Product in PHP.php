30e86f45bed8114141dfe5772167bb18608534d5
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-03-28 19:56:08 +0600

    28/03/2022 last update

---
+++ How to Product in PHP.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>How to Product in PHP</title>
+</head>
+<body>
+     
+
+ 
+ 
+ How to Product in PHP
+
+
+
+01. Fostly = Varyable Diclaration  
+         Note - Minimum Varyable = 2 Data Type 
+
+
+02. Then => Echo 
+
+
+     $number1 = 10;
+     $number2 = 20;
+
+
+
+    Ex:  echo $number1 * $number2
+
+    Ans: 200
+
+
+    </body>
+</html>
